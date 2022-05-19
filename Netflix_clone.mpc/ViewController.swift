bec4dbd0a2dba33d08b943550bac0450d42942de
Matute <matiasperaltacharro@gmail.com>
2022-05-19 11:45:19 -0300

    Initial Commit

---
+++ Netflix_clone.mpc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_clone.mpc
+//
+//  Created by Matias Peralta Charro on 19/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Netflix_clone.mpcTests/Netflix_clone_mpcTests.swift
@@ -0,0 +1,33 @@
+//
+//  Netflix_clone_mpcTests.swift
+//  Netflix_clone.mpcTests
+//
+//  Created by Matias Peralta Charro on 19/05/2022.
+//
+
+import XCTest
+@testable import Netflix_clone_mpc
+
+class Netflix_clone_mpcTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
