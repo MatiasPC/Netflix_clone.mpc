8ea85e6d68d984ed8af0edb0a6efe3ad7aa09f9e
Matute <matiasperaltacharro@gmail.com>
2022-05-19 11:50:24 -0300

    Starting Project (Completly empty)

---
+++ initialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  initialViewController.swift
+//  Netflix_clone.mpc
+//
+//  Created by Matias Peralta Charro on 19/05/2022.
+//
+
+import UIKit
+
+class initialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+//Starting project
+
+}
